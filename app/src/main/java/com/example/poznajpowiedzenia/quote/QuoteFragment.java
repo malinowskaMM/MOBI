30722a7c0ff12851806ae925e6ca06c4289fad81
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-02-06 20:00:39 +0100

    fix speech when activity changed

--- app/src/main/java/com/example/poznajpowiedzenia/quote/QuoteFragment.java
+++ app/src/main/java/com/example/poznajpowiedzenia/quote/QuoteFragment.java
@@ -29,10 +29,15 @@ public class QuoteFragment extends Fragment {
     Button btnSpeak;
     TextToSpeech textToSpeech;
 
+    public TextToSpeech getTextToSpeech() {
+        return textToSpeech;
+    }
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "quote";
     private static final String ARG_PARAM2 = "meaning";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -74,7 +79,16 @@ public class QuoteFragment extends Fragment {
                     textToSpeech.setLanguage(new Locale("pl"));
                 }
             }
-        });
+            });
+    }
+
+    @Override
+    public void onPause(){
+        if(textToSpeech != null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
     }
 
     @Override
